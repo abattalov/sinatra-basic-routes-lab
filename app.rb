58a56ddb0508d5ed4243ecd05255fc75d6673a0d
abattalov <abattalov14@gmail.com>
2021-12-03 16:33:54 -0600

    commit tree 7dcbb89cafac6c2e33a98e96f879f7758a80af3a

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Alex"
+    end
+
+    get '/hometown' do
+        "My hometown is Minneapolis"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Roses"
+    end
 end
